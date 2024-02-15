33da5e4c102b982124ad9b01325c81fe0de964f6
Elie Labeca <1290092+elabeca@users.noreply.github.com>
2024-02-15 11:17:02 +0000

    [RORO-4877][EL] - dummy test endpoints (#14)

--- app/uk/gov/hmrc/customsservicestatusfrontend/controllers/test/TestController.scala
+++ app/uk/gov/hmrc/customsservicestatusfrontend/controllers/test/TestController.scala
@@ -19,16 +19,21 @@ package uk.gov.hmrc.customsservicestatusfrontend.controllers.test
 import com.google.inject._
 import play.api.Logging
 import play.api.mvc._
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.customsservicestatusfrontend.models.State.{AVAILABLE, UNAVAILABLE, UNKNOWN}
 import uk.gov.hmrc.customsservicestatusfrontend.services.test.TestService
+import uk.gov.hmrc.customsservicestatusfrontend.views.html.DashboardPage
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
-import scala.concurrent.ExecutionContext
+import java.time.Instant
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class TestController @Inject() (
+  dashboardPage:   DashboardPage,
   testService:     TestService
-)(implicit val ec: ExecutionContext, cc: MessagesControllerComponents)
-    extends FrontendController(cc)
+)(implicit val ec: ExecutionContext, mcc: MessagesControllerComponents)
+    extends FrontendController(mcc)
     with Logging {
 
   def clearAllData: Action[AnyContent] =
@@ -39,4 +44,16 @@ class TestController @Inject() (
         InternalServerError(s"Failed to clear data: ${e.getMessage}")
       }
     }
+
+  val showAvailable: Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(Ok(dashboardPage(AVAILABLE, Instant.now())))
+  }
+
+  val showUnavailable: Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(Ok(dashboardPage(UNAVAILABLE, Instant.now())))
+  }
+
+  val showUnknown: Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(Ok(dashboardPage(UNKNOWN, Instant.now())))
+  }
 }
