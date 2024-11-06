fb4f6d176e8b72df0f1c029a6d71114b90d351f4
Elie Labeca <1290092+elabeca@users.noreply.github.com>
2024-11-06 09:06:50 +0000

    [RORO-5407][EL] - bump version of play-frontend-hmrc to v11.2.0 (#38)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% s"bootstrap-frontend-$playVersion"         % bootstrapVersion,
-    "uk.gov.hmrc"             %% s"play-frontend-hmrc-$playVersion"         % "10.10.0",
+    "uk.gov.hmrc"             %% s"play-frontend-hmrc-$playVersion"         % "11.2.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                       % hmrcMongoVersion
   )
 
