f2d9ac7c812e3524a50c52bacbd8f6464cd0ec40
Suresh Mandalapu <27204388+sureshhmrc@users.noreply.github.com>
2025-03-12 10:06:04 +0000

    [RORO-5737][SM]: Update bootstrap version (#40)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.7.0"
-  private val hmrcMongoVersion = "2.2.0"
+  private val bootstrapVersion = "9.11.0"
+  private val hmrcMongoVersion = "2.5.0"
   private val playVersion = "play-30"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% s"bootstrap-frontend-$playVersion"         % bootstrapVersion,
-    "uk.gov.hmrc"             %% s"play-frontend-hmrc-$playVersion"         % "11.2.0",
+    "uk.gov.hmrc"             %% s"play-frontend-hmrc-$playVersion"         % "11.12.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                       % hmrcMongoVersion
   )
 
