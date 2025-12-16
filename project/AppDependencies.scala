08780b98c359b7621b6fbd4c0d3f798deab11883
omirostrypatsas <115593155+omirostr1@users.noreply.github.com>
2025-12-16 17:19:04 +0000

    [RORO-6325][OT]upgrades versions - addresses pr bot comment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "9.19.0"
-  private val hmrcMongoVersion = "2.7.0"
+  private val hmrcMongoVersion = "2.11.0"
   private val playVersion = "play-30"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% s"bootstrap-frontend-$playVersion"         % bootstrapVersion,
-    "uk.gov.hmrc"             %% s"play-frontend-hmrc-$playVersion"         % "12.15.0",
+    "uk.gov.hmrc"             %% s"play-frontend-hmrc-$playVersion"         % "12.22.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                       % hmrcMongoVersion
   )
 
