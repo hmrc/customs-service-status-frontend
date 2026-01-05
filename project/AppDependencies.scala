7397d83dc84830885b34763157f09f2faaf44004
omirostrypatsas <115593155+omirostr1@users.noreply.github.com>
2026-01-05 16:27:27 +0000

    [RORO-6325][OT]addresses pr-bot comment to update play frontend version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% s"bootstrap-frontend-$playVersion"         % bootstrapVersion,
-    "uk.gov.hmrc"             %% s"play-frontend-hmrc-$playVersion"         % "12.23.0",
+    "uk.gov.hmrc"             %% s"play-frontend-hmrc-$playVersion"         % "12.25.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                       % hmrcMongoVersion
   )
 
